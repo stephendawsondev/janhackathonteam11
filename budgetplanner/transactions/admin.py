c49d2ac3d0fe5f2b5cddacd35f36c35c37d586a2
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-17 22:39:36 +0000

    Add intial models

--- budgetplanner/transactions/admin.py
+++ budgetplanner/transactions/admin.py
@@ -1,3 +1,9 @@
+# transactions/admin.py
 from django.contrib import admin
+from .models import Budget, Expense, Income
+
+admin.site.register(Budget)
+admin.site.register(Expense)
+admin.site.register(Income)
 
 # Register your models here.
