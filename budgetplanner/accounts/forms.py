bd1562e63f8fad02927759ba0787f2324a05f11d
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-17 23:39:44 +0000

    Add login and logout

---
+++ budgetplanner/accounts/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    indebt = forms.DecimalField(max_digits=10, decimal_places=2)
+    savings = forms.DecimalField(max_digits=10, decimal_places=2)
+    invested = forms.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def save(self, commit=True):
+        user = super(UserRegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+            user_profile = UserProfile.objects.create(
+                user=user,
+                indebt=self.cleaned_data['indebt'],
+                savings=self.cleaned_data['savings'],
+                invested=self.cleaned_data['invested']
+            )
+        return user
--- budgetplanner/budgetplanner/settings.py
+++ budgetplanner/budgetplanner/settings.py
@@ -118,3 +118,5 @@ if not DEBUG:
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'dashboard'
