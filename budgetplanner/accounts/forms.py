9d22d5afb2bea9c582fc5ecefe2a831c7e7df1e2
Stephen Dawson <stephendawson@protonmail.com>
2024-01-18 17:04:15 +0000

    autoformat files on save

--- budgetplanner/accounts/forms.py
+++ budgetplanner/accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile
 
+
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     indebt = forms.DecimalField(max_digits=10, decimal_places=2)
