ee1249264641dc63ca65dbb85385cf5d6b722c8b
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-19 02:09:36 +0000

    Add backup

--- budgetplanner/transactions/migrations/0006_auto_20240119_0145.py
+++ budgetplanner/transactions/migrations/0006_auto_20240119_0145.py
@@ -3,45 +3,14 @@
 from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
-
+from django.db import migrations
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('transactions', '0005_auto_20240118_1718'),
+        ('transactions', '0005_previous_migration_name'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='MonthlyBudget',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('month', models.DateField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='WeeklyBudget',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('start_date', models.DateField()),
-                ('end_date', models.DateField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='YearlyBudget',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
-                ('year', models.DateField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.DeleteModel(
-            name='Budget',
-        ),
+        migrations.RunSQL('ALTER TABLE transactions_monthlybudget ADD COLUMN new_column_name data_type;'),
     ]
