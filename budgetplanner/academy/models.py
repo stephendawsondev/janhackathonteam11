8b6f91d8d625e4f514f7d2e5d418d7efbcd21d21
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2024-01-20 14:43:57 +0000

    feat: academy rss prepare

--- budgetplanner/academy/models.py
+++ budgetplanner/academy/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+
+class ArticleAcademy(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    description = models.TextField(max_length=528)
+    url = models.URLField(max_length=1024, null=True, blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s expense on {self.date}"
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = "ArticleAcademy"
+
+    def __str__(self):
+        return self.title
--- budgetplanner/academy/views.py
+++ budgetplanner/academy/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
+import feedparser
+import textwrap
 
 def academy_articles_view(request):
     return render(request, 'academy_articles.html')
+
+# RSS
+
+
+def my_rss_news(self, url):
+    '''
+    Deal with the fetching and processing of RSS data
+    '''
+    # RSS local variables
+    feed = feedparser.parse(url)
+    top = feed.entries[:3]
+    x = 0
+
+    for news in top:
+        x += 1
+        print(f'\n{BOLD}Coindesk RSS News {x}{RESET}\n')
+
+        # Title
+        title_lines = textwrap.wrap(news.title, width=70)
+        full_title = '\n'.join(title_lines)
+        print(f'{bullet_point} {full_title}:\n')
+
+        # Summary
+        text_lines = textwrap.wrap(news.summary[:200], width=70)
+        # get rid of brackets and add space in between
+        full_text = '\n'.join(text_lines)
+        print(f'{BOLD}{full_text}...{RESET}\n')
+
+        # Link
+        print(f'{bullet_point}', news.link)
