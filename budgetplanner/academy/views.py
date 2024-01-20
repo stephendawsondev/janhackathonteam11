c0af8d0b68411dc6cb3ddc26a10a115f01fb9536
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2024-01-20 15:11:30 +0000

    feat: work on academy

--- budgetplanner/academy/views.py
+++ budgetplanner/academy/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
 import feedparser
-import textwrap
-
-def academy_articles_view(request):
-    return render(request, 'academy_articles.html')
-
+import textwra
 # RSS
 
 
-def my_rss_news(self, url):
+def rss_news(self, url):
     '''
     Deal with the fetching and processing of RSS data
     '''
-    # RSS local variables
+    # Parse URL
     feed = feedparser.parse(url)
     top = feed.entries[:3]
     x = 0
@@ -34,3 +30,19 @@ def my_rss_news(self, url):
 
         # Link
         print(f'{bullet_point}', news.link)
+
+# Article's page
+
+def academy_articles_view(request):
+
+    sources = 
+
+    context = [
+        'origin' = origin,
+        'description' = description,
+        'url' = url,
+        'title' = title,
+        'content' = content,
+        'image_url' = image_url,
+    ]
+    return render(request, 'academy_articles.html', context)
