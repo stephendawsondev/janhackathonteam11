5c61c4bcf7af33c2870c7b60678d6660798e62aa
raneem-yad <eng.raneem.yad@gmail.com>
2024-01-20 12:41:38 +0100

    adding favicons to base.html

--- budgetplanner/transactions/migrations/0001_initial.py
+++ budgetplanner/transactions/migrations/0001_initial.py
@@ -39,18 +39,13 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('amount', models.DecimalField(decimal_places=2, max_digits=10)),
-
                 ('start_date', models.DateField()),
+                ('year', models.DateField()),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'abstract': False,
             },
-=======
-                ('year', models.DateField()),
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-
         ),
         migrations.CreateModel(
             name='WeeklyBudget',
