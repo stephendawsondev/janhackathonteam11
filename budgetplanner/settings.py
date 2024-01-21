a9279850b5bc6ce3e12022c466c576226d1e14e2
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2024-01-21 02:42:01 +0000

    fix: try false

--- budgetplanner/settings.py
+++ budgetplanner/settings.py
@@ -23,7 +23,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
-DEBUG_MODE = os.environ.get('DEBUG_MODE', 'False').lower() == 'true'
+DEBUG_MODE = os.environ.get('DEBUG_MODE', False).lower() == 'true'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG_MODE
