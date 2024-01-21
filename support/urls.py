02e96851749fadbf95d33f09be7d46b8d00fda6e
Stephen Dawson <stephendawson@protonmail.com>
2024-01-21 10:38:17 +0000

    add privacy policy and link

--- support/urls.py
+++ support/urls.py
@@ -1,11 +1,12 @@
 # Django Imports
 from django.urls import path, include
 
-from .views import faq_view, contact_view, about_view, team_view
+from .views import faq_view, contact_view, about_view, team_view, privacy_policy_view
 
 urlpatterns = [
     path('faq/', faq_view, name='faq'),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('team/', team_view, name='team'),
+    path('privacy_policy/', privacy_policy_view, name='privacy_policy'),
 ]
