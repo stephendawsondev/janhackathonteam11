9b46d07440a8eb2ab080921b6aff4901d5bcf44a
Jesse Lee Ross <jesseross001@gmail.com>
2024-01-22 04:13:44 +0000

    fix links on dashboard

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 # Django Imports
 from django.urls import path
 from .views import home
-
+from demo.views import (demo_dashboard_view, demo_income_view,
+                    demo_expenditure_view, demo_manage_budgets)
 urlpatterns = [
     path('', home, name='home'),
 ]
