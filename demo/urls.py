f0be04ece761cb8c6f262bbbbd58eba8e1e3dcd9
Frank Reinaldo Arellano Andrade <plexonft@gmail.com>
2024-01-22 02:32:08 +0000

    feat: demo almost complete

--- demo/urls.py
+++ demo/urls.py
@@ -1,10 +1,13 @@
 # Django Imports
 from django.urls import path
-from .views import demo_dashboard_view, demo_income_view, demo_expenditure_view
+from .views import (demo_dashboard_view, demo_income_view,
+                    demo_expenditure_view, demo_manage_budgets)
 
 urlpatterns = [
     path('demo/', demo_dashboard_view, name='demo_dashboard'),
     path('demo/income', demo_income_view, name='demo_income'),
-    path('demo/expenditure', demo_expenditure_view, name='demo_expenditure'),
+    path('demo/expenditure', demo_expenditure_view, name='demo_expenditure_view'),
+    path('demo/manage_budgets', demo_manage_budgets, name='demo_manage_budgets'),
+
 
 ]
